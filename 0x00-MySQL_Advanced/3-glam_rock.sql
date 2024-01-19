7d01efbf9f470879a30b74a973948faa7c519c52
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2024-01-19 21:28:45 +0300

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,7 @@
 -- Column names must be: band_name and lifespan (in years)
 -- use attributes formed and split for computing the lifespan
 
+
 SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM
 metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+
